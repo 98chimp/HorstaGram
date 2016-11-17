1217972a33c0d915f6b80468c74e2ad391161e7b
Shahin Zangenehpour <shahin@98chimp.com>
2016-11-16 17:01:25 -0800

    Initial Commit

---
+++ HorstaGram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorstaGram
+//
+//  Created by Shahin on 2016-11-16.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorstaGram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorstaGram
+//
+//  Created by Shahin on 2016-11-16.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
